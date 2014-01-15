31e990140a9043b0e373e9eb63d7244865c0329f
Vectovox <robert-kaufmann@live.com>
2014-01-15 16:58:10 +0100

    Added extension methods for Top-Left and Bottom-Right in Microsoft.Framework.Rectangle.

---
+++ MonoKle/Core/RectangleExtension.cs
@@ -0,0 +1,33 @@
+﻿namespace MonoKle.Core
+{
+    using Microsoft.Xna.Framework;
+
+    public static class RectangleExtension
+    {
+        /*
+         * NOTE: Hopefully Microsoft will add property extensions sooner rather than later to C#.
+         * This would be nice to have since that would follow the original Rectangle format of
+         * Top/Bottom/Left/Right.
+         */
+
+        /// <summary>
+        /// Gets the bottom right coordinate.
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <returns></returns>
+        public static Vector2Int32 GetBottomRight(this Rectangle rectangle)
+        {
+            return new Vector2Int32(rectangle.Right, rectangle.Bottom);
+        }
+
+        /// <summary>
+        /// Gets the top left coordinate.
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <returns></returns>
+        public static Vector2Int32 GetTopLeft(this Rectangle rectangle)
+        {
+            return new Vector2Int32(rectangle.Left, rectangle.Top);
+        }
+    }
+}

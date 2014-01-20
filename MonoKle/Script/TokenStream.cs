ce46ba732092777ec0e5742e1effc20517f95b5a
Vectovox <robert-kaufmann@live.com>
2014-01-20 01:19:37 +0100

    Rudimentary script engine written. Scripts can return values and evaluate expressions.

---
+++ MonoKle/Script/TokenStream.cs
@@ -0,0 +1,40 @@
+﻿namespace MonoKle.Script
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    internal class TokenStream
+    {
+        private Dictionary<string, int> tokenByString = new Dictionary<string, int>();
+
+        private LinkedList<int> tokens = new LinkedList<int>();
+
+        public TokenStream()
+        {
+            tokenByString.Add("return", 1);
+            tokenByString.Add("set", 2);
+        }
+
+        public void Tokenize(string[] source)
+        {
+            for(int i = 0; i < source.Length; i++)
+            {
+                if(tokenByString.ContainsKey(source[i]))
+                {
+                    tokens.AddLast(tokenByString[source[i]]);
+                }
+                else
+                {
+                    tokens.AddLast(0);
+                }
+            }
+        }
+
+        public int[] GetTokens()
+        {
+            return tokens.ToArray();
+        }
+    }
+}

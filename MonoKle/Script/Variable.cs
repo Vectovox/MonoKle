ce2d2dcc50cf676bea37489fa898e0079520c1ac
Vectovox <robert-kaufmann@live.com>
2014-01-21 18:51:18 +0100

    Added print function and enabled initiation/setting/use of variables.

--- MonoKle/Script/Variable.cs
+++ MonoKle/Script/Variable.cs
@@ -5,5 +5,11 @@
     {
         public Type type;
         public byte number;
+
+        public Variable(Type type, byte number)
+        {
+            this.type = type;
+            this.number = number;
+        }
     }
 }

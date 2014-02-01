218404e355412d15814b3ff3c819def2494e1c71
Vectovox <robert-kaufmann@live.com>
2014-02-01 20:04:41 +0100

    Scripts can now call other scripts. Refactored a lot of code.

---
+++ MonoKle/Scripting/Token.cs
@@ -0,0 +1,34 @@
+﻿namespace MonoKle.Scripting
+{
+    using System;
+
+    internal struct Token
+    {
+        public string text;
+        public byte hiearchyValue;
+        public bool isLogicOperator;
+        public bool isArithmeticOperator;
+        public bool isFunction;
+        public bool isOperand;
+        public bool isGroupingLeft;
+        public bool isGroupingRight;
+        public Type operandType;
+        public string functionName;
+        public Token[][] functionArguments;
+        
+        public bool IsOperator()
+        {
+            return this.isArithmeticOperator || this.isLogicOperator;
+        }
+
+        public bool IsGrouping()
+        {
+            return this.isGroupingLeft || this.isGroupingRight;
+        }
+
+        public bool IsOperatorGrouping()
+        {
+            return this.IsOperator() || this.IsGrouping();
+        }
+    }
+}

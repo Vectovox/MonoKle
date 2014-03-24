34e8ffad98164050d7fa4f3d7795f219011d947c
Vectovox <robert-kaufmann@live.com>
2014-03-24 19:58:02 +0100

    First steps on an ANTLR based compiler.

---
+++ GrammarTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MonoKleScript.Compiler;
+using MonoKleScript.Script;
+
+namespace GrammarTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"int x : 5 int y : 5";
+            
+            ScriptCompiler c = new ScriptCompiler();
+            c.CompilationError += c_CompilationError;
+            c.Compile(new ScriptSource(source, new ScriptHeader("name", typeof(void), null, new ScriptVariable[0])), new LinkedList<ScriptHeader>());
+
+            MonoKleScript.Debug.LanguageDebugger.PrintLexerTokens(source);
+            MonoKleScript.Debug.LanguageDebugger.PrintParserTree(source);
+            MonoKleScript.Debug.LanguageDebugger.PrintParserTokens(source);
+
+            Console.Read();
+        }
+
+        static void c_CompilationError(object sender, CompilationErrorEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}

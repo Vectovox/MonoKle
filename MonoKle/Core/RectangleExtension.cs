fc6db8b6ce4ae49e6080aef0ea616f7f23487120
unknown <robert-kaufmann@live.com>
2014-04-26 13:12:08 +0200

    Added more extension methods for Rectangles and made the extension class internal.

--- MonoKle/Core/RectangleExtension.cs
+++ MonoKle/Core/RectangleExtension.cs
@@ -2,13 +2,23 @@
 {
     using Microsoft.Xna.Framework;
 
-    public static class RectangleExtension
+    /*
+     * NOTE: Hopefully Microsoft will add property extensions sooner rather than later to C#.
+     * This would be nice to have since that would follow the original Rectangle format of
+     * Top/Bottom/Left/Right-properties.
+     */
+    internal static class RectangleExtension
     {
-        /*
-         * NOTE: Hopefully Microsoft will add property extensions sooner rather than later to C#.
-         * This would be nice to have since that would follow the original Rectangle format of
-         * Top/Bottom/Left/Right-properties.
-         */
+        /// <summary>
+        /// Gets the bottom left coordinate.
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <returns></returns>
+        public static Vector2Int32 GetBottomLeft(this Rectangle rectangle)
+        {
+            return new Vector2Int32(rectangle.Left, rectangle.Bottom);
+        }
+
         /// <summary>
         /// Gets the bottom right coordinate.
         /// </summary>
@@ -27,6 +37,16 @@
         public static Vector2Int32 GetTopLeft(this Rectangle rectangle)
         {
             return new Vector2Int32(rectangle.Left, rectangle.Top);
+        }
+
+        /// <summary>
+        /// Gets the top right coordinate.
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <returns></returns>
+        public static Vector2Int32 GetTopRight(this Rectangle rectangle)
+        {
+            return new Vector2Int32(rectangle.Right, rectangle.Top);
         }
     }
 }

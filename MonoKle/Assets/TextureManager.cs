9da45efde3825c4c0bf5cd71d642942b68ccd4a3
unknown <robert-kaufmann@live.com>
2014-06-09 22:52:29 +0200

    Updated TextureManager to be able to load textures into groups. Textures can now be retrieved per group.

--- MonoKle/Assets/TextureManager.cs
+++ MonoKle/Assets/TextureManager.cs
@@ -19,55 +19,129 @@
         public Texture2D DefaultTexture { get; set; }
         public Texture2D WhiteTexture { get; private set; }
 
-        private Dictionary<string, Texture2D> textureStorage = new Dictionary<string, Texture2D>();
+        private Dictionary<string, Texture2D> textureByTextureName = new Dictionary<string, Texture2D>();
         private GraphicsDevice graphicsDevice;
 
         public TextureManager(GraphicsDevice graphicsDevice)
         {
             this.graphicsDevice = graphicsDevice;
-            DefaultTexture = GraphicsHelper.BitmapToTexture2D(graphicsDevice, TextureResources.DefaultTexture);
-            WhiteTexture = GraphicsHelper.BitmapToTexture2D(graphicsDevice, TextureResources.WhiteTexture);
+            this.DefaultTexture = GraphicsHelper.BitmapToTexture2D(graphicsDevice, TextureResources.DefaultTexture);
+            this.WhiteTexture = GraphicsHelper.BitmapToTexture2D(graphicsDevice, TextureResources.WhiteTexture);
         }
 
+        /// <summary>
+        /// Returns a dictionary containing all loaded textures and their id keys.
+        /// </summary>
+        /// <returns>Dictionary with id-texture pairs.</returns>
+        public Dictionary<string, Texture2D> GetTextures()
+        {
+            return new Dictionary<string, Texture2D>(this.textureByTextureName);
+        }
+
+        /// <summary>
+        /// Returns a dictionary containing all loaded textures in the given group, as well as their id keys.
+        /// </summary>
+        /// <param name="group">The group to get textures from.</param>
+        /// <returns>Dictionary with id-texture pairs.</returns>
+        public Dictionary<string, Texture2D> GetTextures(string group)
+        {
+            Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
+            if(this.groupByGroupName.ContainsKey(group))
+            {
+                foreach(string s in this.groupByGroupName[group])
+                {
+                    textures.Add(s, this.textureByTextureName[s]);
+                }
+            }
+            return textures;
+        }
+
+        /// <summary>
+        /// Returns a dictionary containing all texture groups and their corresponding id keys.
+        /// </summary>
+        /// <returns>Dictionary.</returns>
+        public Dictionary<string, HashSet<string>> GetTextureGroups()
+        {
+            return new Dictionary<string, HashSet<string>>(this.groupByGroupName);
+        }
+
+        /// <summary>
+        /// Retrieves the texture with the given id at constant time. If no texture exists with the given id, <see cref="DefaultTexture"/> is returned.
+        /// </summary>
+        /// <param name="id">The id of the texture to retrieve.</param>
+        /// <returns>Texture2D</returns>
         public Texture2D GetTexture(string id)
         {
             id = id.ToLower();
-            if (textureStorage.ContainsKey(id))
+            if (this.textureByTextureName.ContainsKey(id))
             {
-                return textureStorage[id];
+                return this.textureByTextureName[id];
             }
-            return DefaultTexture;
+            return this.DefaultTexture;
         }
 
         public int Load(string path)
         {
-            return Load(path, false);
+            return this.Load(path, false);
         }
 
         public int Load(string path, bool recurse)
         {
+            return this.Load(path, recurse, DEFAULT_GROUP_NAME);
+        }
+
+        public int Load(string path, bool recurse, string group)
+        {
             int nLoaded = 0;
-            if (Directory.Exists(path))
+            if(Directory.Exists(path))
             {
-                nLoaded = LoadDirectory(path, recurse);
+                nLoaded = this.LoadDirectory(path, recurse, group);
             }
             else
             {
-                nLoaded = LoadFile(path);
+                nLoaded = this.LoadFile(path, group);
             }
-            Logger.GetGlobalInstance().AddLog("Texture loading (" + path + ") complete. " + nLoaded + " texture(s) loaded.", LogLevel.Info);
+            Logger.GetGlobalInstance().AddLog("Texture loading (" + path + ") complete. " + nLoaded + " texture(s) loaded into group " + group + ".", LogLevel.Info);
             return nLoaded;
         }
 
-        public int Unload(string id)
+        public int UnloadTexture(string id)
         {
-            return textureStorage.Remove(id) ? 1 : 0;
+            if(this.textureByTextureName.ContainsKey(id))
+            {
+                string groupName = this.groupNameByTextureName[id];
+                this.groupByGroupName[groupName].Remove(id);
+                // Remove group if empty
+                if(this.groupByGroupName[groupName].Count == 0)
+                {
+                    this.groupByGroupName.Remove(groupName);
+                }
+                this.textureByTextureName.Remove(id);
+                return 1;
+            }
+            return 0;
         }
 
-        public int UnloadAll()
+        public int UnloadGroup(string group)
         {
-            int nUnloaded = textureStorage.Keys.Count;
-            textureStorage.Clear();
+            int nUnloaded = 0;
+            if(this.groupByGroupName.ContainsKey(group))
+            {
+                HashSet<string> groupContent = new HashSet<string>(this.groupByGroupName[group]);
+                foreach(string s in groupContent)
+                {
+                    nUnloaded += this.UnloadTexture(s);
+                }
+            }
+            return nUnloaded;
+        }
+
+        public int UnloadTextures()
+        {
+            int nUnloaded = this.textureByTextureName.Keys.Count;
+            this.textureByTextureName.Clear();
+            this.groupByGroupName.Clear();
+            this.groupNameByTextureName.Clear();
             return nUnloaded;
         }
 
@@ -89,7 +163,7 @@
                 || path.EndsWith(".png", StringComparison.CurrentCultureIgnoreCase);
         }
 
-        private int LoadDirectory(string path, bool recurse)
+        private int LoadDirectory(string path, bool recurse, string group)
         {
             int nLoaded = 0;
             if (Directory.Exists(path))
@@ -97,7 +171,7 @@
                 // Load files
                 foreach (string s in Directory.GetFiles(path))
                 {
-                    nLoaded += LoadFile(s);
+                    nLoaded += LoadFile(s, group);
                 }
 
                 if (recurse)
@@ -105,32 +179,56 @@
                     // Recursively load directories
                     foreach (string s in Directory.GetDirectories(path))
                     {
-                        nLoaded += LoadDirectory(s, recurse);
+                        nLoaded += this.LoadDirectory(s, recurse, group);
                     }
                 }
             }
             return nLoaded;
         }
 
-        private int LoadFile(string path)
+        private int LoadFile(string path, string group)
         {
-            if (File.Exists(path) && IsCompatible(path))
+            if(File.Exists(path) && this.IsCompatible(path))
             {
-                String id = GetIdentifier(path).ToLower();
-                if (id != null && textureStorage.ContainsKey(id) == false)
+                String id = this.GetIdentifier(path).ToLower();
+                if(id != null && this.textureByTextureName.ContainsKey(id) == false)
                 {
                     FileStream stream = File.OpenRead(path);
-                    Texture2D tex = Texture2D.FromStream(graphicsDevice, stream);
+                    Texture2D tex = Texture2D.FromStream(this.graphicsDevice, stream);
                     stream.Close();
 
                     if (tex != null)
                     {
-                        textureStorage.Add(id, tex);
+                        this.textureByTextureName.Add(id, tex);
+                        this.AddToGroup(group, id);
                         return 1;
                     }
                 }
             }
             return 0;
         }
+
+        private void AddToGroup(string group, string textureId)
+        {
+            if(this.textureByTextureName.ContainsKey(textureId))
+            {
+                if(this.groupByGroupName.ContainsKey(group) == false)
+                {
+                    this.groupByGroupName.Add(group, new HashSet<string>());
+                }
+
+                var groupSet = this.groupByGroupName[group];
+
+                if(groupSet.Contains(textureId) == false)
+                {
+                    groupSet.Add(textureId);
+                    this.groupNameByTextureName.Add(textureId, group);
+                }
+            }
+        }
+
+        private Dictionary<string, HashSet<string>> groupByGroupName = new Dictionary<string, HashSet<string>>();
+        private Dictionary<string, string> groupNameByTextureName = new Dictionary<string, string>();
+        private const string DEFAULT_GROUP_NAME = "default";
     }
 }

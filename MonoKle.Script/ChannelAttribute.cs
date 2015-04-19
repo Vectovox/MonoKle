8e3ff42b1489fb9db3ad0d9c7e1c35cb63d01cf5
unknown <robert-kaufmann@live.com>
2015-04-19 20:17:53 +0200

    Added channel functionality for scripts.

---
+++ MonoKle.Script/ChannelAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace MonoKle.Script
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class ChannelAttribute : Attribute
+    {
+        public string Channel { get; set; }
+
+        public ChannelAttribute(string channel)
+        {
+            this.Channel = channel;
+        }
+    }
+}

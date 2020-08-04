46fd5b6ac884ebc116aaa040625e6f5147afcfb6
Robert Kaufmann <robert-kaufmann@live.com>
2020-08-04 13:21:34 +0200

    Refactor MonoKleGame code

--- MonoKle.Engine/MonoKleGame.cs
+++ MonoKle.Engine/MonoKleGame.cs
@@ -19,132 +19,95 @@ namespace MonoKle.Engine
     /// </summary>
     public class MonoKleGame : Game
     {
-        private static GameConsole console;
-        private static GamePadHub gamepad;
-        private static bool initializing;
-        private static Keyboard keyboard;
-        private static Mouse mouse;
-        private static readonly MonoKleSettings settings = new MonoKleSettings();
-        private static StateSystem stateSystem;
-        private static TouchScreen touchScreen = new TouchScreen();
+        private static bool initializing = true;
 
         /// <summary>
         /// Gets the game console, printing logs and accepting input.
         /// </summary>
-        public static IGameConsole Console => MonoKleGame.console;
+        public static IGameConsole Console => console;
+        private static GameConsole console;
 
         /// <summary>
         /// Gets the effect storage, loading and providing effects.
         /// </summary>
-        public static EffectStorage EffectStorage
-        {
-            get;
-            private set;
-        }
+        public static EffectStorage EffectStorage { get; private set; }
 
         /// <summary>
         /// Gets the font storage, loading and providing fonts.
         /// </summary>
-        public static FontStorage FontStorage
-        {
-            get;
-            private set;
-        }
+        public static FontStorage FontStorage { get; private set; }
 
         /// <summary>
-        /// Gets the hub for gamepads.
+        /// Gets the graphics manager. This is in charge of screen settings (resolution, full-screen, etc.)
+        /// and provides the <see cref="GraphicsDevice"/>.
         /// </summary>
-        public static IGamePadHub GamePad => MonoKleGame.gamepad;
-
-        /// <summary>
-        /// Gets the graphics manager. This is in charge of screen settings (resolution, full-screen, etc.) and provides the <see cref="GraphicsDevice"/>.
-        /// </summary>
-        public static GraphicsManager GraphicsManager
-        {
-            get;
-            private set;
-        }
+        public static GraphicsManager GraphicsManager { get; private set; }
 
         /// <summary>
         /// Gets wether the game is running slowly or not.
         /// </summary>
-        public static bool IsRunningSlowly
-        {
-            get;
-            private set;
-        }
+        public static bool IsRunningSlowly { get; private set; }
+
+        /// <summary>
+        /// Gets the hub for gamepad input.
+        /// </summary>
+        public static IGamePadHub GamePad => gamepad;
+        private static readonly GamePadHub gamepad = new GamePadHub();
 
         /// <summary>
         /// Gets the keyboard input.
         /// </summary>
-        public static IKeyboard Keyboard => MonoKleGame.keyboard;
+        public static IKeyboard Keyboard => keyboard;
+        private static readonly Keyboard keyboard = new Keyboard();
 
         /// <summary>
-        /// Gets the logging utility, same as <see cref="Logger.Global"/>.
-        /// </summary>
-        public static Logger Logger
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// Gets the current mouse.
-        /// </summary>
-        public static IMouse Mouse => MonoKleGame.mouse;
-
-        /// <summary>
-        /// Gets the touch screen.
+        /// Gets the touch screen input.
         /// </summary>
         public static ITouchScreen TouchScreen => touchScreen;
+        private static readonly TouchScreen touchScreen = new TouchScreen();
+
+        /// <summary>
+        /// Gets the current mouse input.
+        /// </summary>
+        public static IMouse Mouse => mouse;
+        private static readonly Mouse mouse = new Mouse();
 
         /// <summary>
         /// Gets the state system, which keeps track of the states and switches between them.
         /// </summary>
-        public static IStateSystem StateSystem => MonoKleGame.stateSystem;
+        public static IStateSystem StateSystem => stateSystem;
+        private static readonly StateSystem stateSystem = new StateSystem();
 
         /// <summary>
         /// Gets the running game instance.
         /// </summary>
-        public static MonoKleGame GameInstance { get; private set; }
+        public static MonoKleGame GameInstance { get; } = new MonoKleGame();
+
+        /// <summary>
+        /// Gets or sets the global game settings.
+        /// </summary>
+        public static MonoKleSettings Settings { get; } = new MonoKleSettings();
+
+        /// <summary>
+        /// Gets the logging utility, same as <see cref="Logger.Global"/>.
+        /// </summary>
+        public static Logger Logger { get; private set; }
 
         /// <summary>
         /// Gets the texture storage, loading and providing textures.
         /// </summary>
-        public static TextureStorage TextureStorage
-        {
-            get;
-            private set;
-        }
+        public static TextureStorage TextureStorage { get; private set; }
 
         /// <summary>
         /// Gets the total time spent in the game.
         /// </summary>
-        public static TimeSpan TotalGameTime
-        {
-            get;
-            private set;
-        }
+        public static TimeSpan TotalGameTime { get; private set; }
 
         /// <summary>
-        /// Gets the game variables, including settings.
+        /// Gets the variable storage, handling all game variables, such as settings.
         /// </summary>
         /// <value>
-        /// The variables.
-        /// </value>
-        public static VariableStorage Variables
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// Gets or sets the settings.
-        /// </summary>
-        /// <value>
-        /// The settings.
-        /// </value>
-        public static MonoKleSettings Settings => MonoKleGame.settings;
+        public static VariableStorage Variables { get; private set; }
 
         /// <summary>
         /// Initializes the MonoKle backend, returning a runnable game instance.
@@ -153,29 +116,21 @@ namespace MonoKle.Engine
         /// <param name="fullscreen">The initial fullscreen setting.</param>
         public static MonoKleGame Create(bool fullscreen)
         {
-            MonoKleGame.initializing = true;
-            
-            // Create the singleton instance
-            MonoKleGame.GameInstance = new MonoKleGame();
-
             // Graphics device has to be created immediately but cannot be used before LoadContent
-            MonoKleGame.GraphicsManager = new GraphicsManager(new GraphicsDeviceManager(GameInstance));
-            MonoKleGame.GraphicsManager.ResolutionChanged += ResolutionChanged;
-            MonoKleGame.GraphicsManager.IsFullscreen = fullscreen;
+            GraphicsManager = new GraphicsManager(new GraphicsDeviceManager(GameInstance));
+            GraphicsManager.ResolutionChanged += ResolutionChanged;
+            GraphicsManager.IsFullscreen = fullscreen;
 
-            MonoKleGame.settings.GamePadEnabled = true;
-            MonoKleGame.settings.KeyboardEnabled = true;
-            MonoKleGame.settings.MouseEnabled = true;
+            Settings.GamePadEnabled = true;
+            Settings.KeyboardEnabled = true;
+            Settings.MouseEnabled = true;
+            Settings.TouchEnabled = true;
 
             // Set logger and enabled crashdumps
-            MonoKleGame.Logger = Logger.Global;
+            Logger = Logger.Global;
             AppDomain.CurrentDomain.UnhandledException += UnhandledException;
 
-            MonoKleGame.InitializeVariables();
-            MonoKleGame.gamepad = new GamePadHub();
-            MonoKleGame.keyboard = new Keyboard();
-            MonoKleGame.mouse = new Mouse();
-            MonoKleGame.stateSystem = new StateSystem();
+            InitializeVariables();
 
             return GameInstance;
         }
@@ -184,121 +139,121 @@ namespace MonoKle.Engine
         {
             base.LoadContent();
 
-            MonoKleGame.InitializeTextureStorage();
-            MonoKleGame.InitializeFontStorage();
-            MonoKleGame.EffectStorage = new EffectStorage(GraphicsManager.GraphicsDevice);
-            MonoKleGame.InitializeConsole();
+            InitializeTextureStorage();
+            InitializeFontStorage();
+            EffectStorage = new EffectStorage(GraphicsManager.GraphicsDevice);
+            InitializeConsole();
             Console.CommandBroker.RegisterCallingAssembly();
-            MonoKleGame.BindSettings();
+            BindSettings();
 
             mouse.VirtualRegion = new MRectangleInt(GraphicsManager.Resolution);
 
             // Done initializing
-            console.WriteLine("MonoKle Engine initialized!", MonoKleGame.Console.CommandTextColour);
-            console.WriteLine("Running version: " + Assembly.GetAssembly(typeof(MonoKleGame)).GetName().Version, MonoKleGame.Console.CommandTextColour);
-            MonoKleGame.initializing = false;
+            console.WriteLine("MonoKle Engine initialized!", Console.CommandTextColour);
+            console.WriteLine("Running version: " + Assembly.GetAssembly(typeof(MonoKleGame)).GetName().Version, Console.CommandTextColour);
+            initializing = false;
         }
 
         protected override void Draw(GameTime time)
         {
-            if (MonoKleGame.initializing == false)
+            if (initializing == false)
             {
                 var deltaTime = time.ElapsedGameTime;
 
-                MonoKleGame.GraphicsManager.GraphicsDevice.Clear(Color.CornflowerBlue);
-                MonoKleGame.stateSystem.Draw(deltaTime);
+                GraphicsManager.GraphicsDevice.Clear(Color.CornflowerBlue);
+                stateSystem.Draw(deltaTime);
 
-                if (MonoKleGame.settings.ConsoleEnabled)
+                if (Settings.ConsoleEnabled)
                 {
-                    MonoKleGame.console.Draw(deltaTime);
+                    console.Draw(deltaTime);
                 }
             }
         }
 
         protected override void Update(GameTime time)
         {
-            if (MonoKleGame.initializing == false)
+            if (initializing == false)
             {
                 var deltaTime = time.ElapsedGameTime;
-                MonoKleGame.IsRunningSlowly = time.IsRunningSlowly;
-                MonoKleGame.TotalGameTime = time.TotalGameTime;
+                IsRunningSlowly = time.IsRunningSlowly;
+                TotalGameTime = time.TotalGameTime;
 
-                if (MonoKleGame.settings.GamePadEnabled)
+                if (Settings.GamePadEnabled)
                 {
-                    MonoKleGame.gamepad.Update(deltaTime);
+                    gamepad.Update(deltaTime);
                 }
 
-                if (MonoKleGame.settings.KeyboardEnabled)
+                if (Settings.KeyboardEnabled)
                 {
-                    MonoKleGame.keyboard.Update(deltaTime);
+                    keyboard.Update(deltaTime);
                 }
 
-                if (MonoKleGame.settings.MouseEnabled)
+                if (Settings.MouseEnabled)
                 {
-                    MonoKleGame.mouse.Update(deltaTime);
+                    mouse.Update(deltaTime);
                 }
 
-                if (MonoKleGame.settings.TouchEnabled)
+                if (Settings.TouchEnabled)
                 {
                     touchScreen.Update(deltaTime);
                 }
 
                 GraphicsManager.Update(Window.ClientBounds.Size);
 
-                if (MonoKleGame.Console.IsOpen == false)
+                if (Console.IsOpen == false)
                 {
-                    MonoKleGame.stateSystem.Update(deltaTime);
+                    stateSystem.Update(deltaTime);
                 }
 
-                if (MonoKleGame.settings.ConsoleEnabled)
+                if (Settings.ConsoleEnabled)
                 {
-                    MonoKleGame.console.Update(deltaTime);
+                    console.Update(deltaTime);
                 }
             }
         }
 
         private static void BindSettings()
         {
-            MonoKleGame.Variables.Variables.BindProperties(MonoKleGame.Logger);
-            MonoKleGame.Variables.Variables.BindProperties(MonoKleGame.GraphicsManager);
-            MonoKleGame.Variables.Variables.BindProperties(MonoKleGame.Console);
-            MonoKleGame.Variables.Variables.BindProperties(MonoKleGame.Settings);
-            MonoKleGame.Variables.Variables.BindProperties(MonoKleGame.Mouse);
+            Variables.Variables.BindProperties(Logger);
+            Variables.Variables.BindProperties(GraphicsManager);
+            Variables.Variables.BindProperties(Console);
+            Variables.Variables.BindProperties(Settings);
+            Variables.Variables.BindProperties(Mouse);
         }
 
         private static void InitializeConsole()
         {
-            MonoKleGame.console = new GameConsole(new Rectangle(0, 0, GraphicsManager.Resolution.X, GraphicsManager.Resolution.Y / 3),
-                MonoKleGame.GraphicsManager.GraphicsDevice,
-                MonoKleGame.keyboard,
-                MonoKleGame.TextureStorage.White,
-                MonoKleGame.FontStorage.DefaultValue,
-                MonoKleGame.Logger);
-            MonoKleGame.Console.ToggleKey = Microsoft.Xna.Framework.Input.Keys.F1;
-            MonoKleGame.Console.TextFont = MonoKleGame.FontStorage.DefaultValue;
+            console = new GameConsole(new Rectangle(0, 0, GraphicsManager.Resolution.X, GraphicsManager.Resolution.Y / 3),
+                GraphicsManager.GraphicsDevice,
+                keyboard,
+                TextureStorage.White,
+                FontStorage.DefaultValue,
+                Logger);
+            Console.ToggleKey = Microsoft.Xna.Framework.Input.Keys.F1;
+            Console.TextFont = FontStorage.DefaultValue;
         }
 
         private static void InitializeFontStorage()
         {
-            MonoKleGame.FontStorage = new FontStorage(GraphicsManager.GraphicsDevice);
+            FontStorage = new FontStorage(GraphicsManager.GraphicsDevice);
             using var ms = new MemoryStream(Resources.FontResources.DefaultFont);
-            MonoKleGame.FontStorage.Load(ms, "default");
-            MonoKleGame.FontStorage.DefaultValue = MonoKleGame.FontStorage.GetAsset("default");
+            FontStorage.Load(ms, "default");
+            FontStorage.DefaultValue = FontStorage.GetAsset("default");
         }
 
-        private static void InitializeTextureStorage() => MonoKleGame.TextureStorage = new TextureStorage(GraphicsManager.GraphicsDevice);
+        private static void InitializeTextureStorage() => TextureStorage = new TextureStorage(GraphicsManager.GraphicsDevice);
 
         private static void InitializeVariables()
         {
-            MonoKleGame.Variables = new VariableStorage(MonoKleGame.Logger);
-            MonoKleGame.Variables.LoadDefaultVariables();
+            Variables = new VariableStorage(Logger);
+            Variables.LoadDefaultVariables();
         }
 
         private static void ResolutionChanged(object sender, ResolutionChangedEventArgs e)
         {
-            if (MonoKleGame.Console != null)
+            if (Console != null)
             {
-                MonoKleGame.Console.Area = new Rectangle(0, 0, MonoKleGame.GraphicsManager.ResolutionWidth, MonoKleGame.GraphicsManager.ResolutionHeight / 3);
+                Console.Area = new Rectangle(0, 0, GraphicsManager.ResolutionWidth, GraphicsManager.ResolutionHeight / 3);
             }
         }
 

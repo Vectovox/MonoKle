751434a5f2465923d38ecacd5e14429dc0022270
Robert Kaufmann <robert-kaufmann@live.com>
2020-08-06 16:37:48 +0200

    Add script to push MonoKle to github

---
+++ push-package.sh
@@ -0,0 +1,18 @@
+push_package()
+{
+	PACKAGE=$1
+	echo "Pushing package $PACKAGE"
+	dotnet nuget push $PACKAGE -s https://nuget.pkg.github.com/Vectovox/index.json
+}
+
+# Main body
+if [ -z "$1" ]
+then
+  echo "You need to provide a version (e.g. 0.3.5-alpha.001)"
+else
+  VERSION=$1
+  echo "Pushing version: $VERSION"
+  push_package ./MonoKle/bin/Release/MonoKle.$VERSION.nupkg
+  push_package ./MonoKle.Engine/bin/Release/MonoKle.Engine.$VERSION.nupkg
+  push_package ./MonoKle.Utilities/bin/Release/MonoKle.Utilities.$VERSION.nupkg
+fi

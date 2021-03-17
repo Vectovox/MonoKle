053b008ead739b3e56e5a787f975b882e3280ebf
Robert Kaufmann <robert-kaufmann@live.com>
2021-03-17 11:33:19 +0100

    Fix broken tests due to change in contract.

--- MonoKle.Tests/Variable/VariablePopulatorTests.cs
+++ MonoKle.Tests/Variable/VariablePopulatorTests.cs
@@ -16,86 +16,35 @@ namespace MonoKle.Configuration.Tests
         }
 
         [TestMethod]
-        public void LoadText_CommentedLine()
+        public void LoadText_CommentedLine_NoChange()
         {
             LoadTextLine("test", "5", true);
             Assert.AreEqual(0, _system.Identifiers.Count);
         }
 
         [TestMethod]
-        public void LoadText_MPoint2Line()
+        public void LoadText_EmptyString_Works()
         {
-            var p = new MPoint2(-27, 89);
-            LoadTextLine("test", p.ToString(), false);
-            Assert.AreEqual(p, _system.GetValue("test"));
+            LoadTextLine("a", "", false);
+            Assert.AreEqual("", _system.GetValue("a"));
         }
 
         [TestMethod]
-        public void LoadText_MVector2Line()
+        public void LoadText_Whitespace_Trimmed()
         {
-            var p = new MVector2(-27.7f, 89.23f);
-            LoadTextLine("test", p.ToString(), false);
-            Assert.AreEqual(p, _system.GetValue("test"));
-        }
-
-        [TestMethod]
-        public void LoadText_FloatLine()
-        {
-            LoadTextLine("test", "5.8", false);
-            Assert.AreEqual(5.8f, _system.GetValue("test"));
-        }
-
-        [TestMethod]
-        public void LoadText_IntLine()
-        {
-            LoadTextLine("test", "5", false);
-            Assert.AreEqual(5, _system.GetValue("test"));
-        }
-
-        [TestMethod]
-        public void LoadText_Whitespace_Works()
-        {
-            _populator.LoadText("     a     =     7");
-            Assert.AreEqual(7, _system.GetValue("a"));
+            _populator.LoadText("     a     =     bepa");
+            Assert.AreEqual("bepa", _system.GetValue("a"));
         }
 
         [TestMethod]
         public void LoadText_Multiline()
         {
-            string text = "a = 5\nb=2.5\n" + CVarFileLoader.CommentedLineToken + "kaka=5\nc=\"hej\"";
+            string text = "a = 5\nb=2.5\n" + CVarFileLoader.CommentedLineToken + "kaka=5\nc=hej";
             _populator.LoadText(text);
-            Assert.AreEqual(5, _system.GetValue("a"));
-            Assert.AreEqual(2.5f, _system.GetValue("b"));
+            Assert.AreEqual("5", _system.GetValue("a"));
+            Assert.AreEqual("2.5", _system.GetValue("b"));
             Assert.AreEqual("hej", _system.GetValue("c"));
             Assert.AreEqual(3, _system.Identifiers.Count);
-        }
-
-        [TestMethod]
-        public void LoadText_StringLine()
-        {
-            LoadTextLine("test", "\"apa\"", false);
-            Assert.AreEqual("apa", _system.GetValue("test"));
-        }
-
-        [TestMethod]
-        public void LoadText_TrueLine_CaseIgnored()
-        {
-            LoadTextLine("test", "tRuE", false);
-            Assert.AreEqual(true, _system.GetValue("test"));
-        }
-
-        [TestMethod]
-        public void LoadText_FalseLine_CaseIgnored()
-        {
-            LoadTextLine("test", "fAlSe", false);
-            Assert.AreEqual(false, _system.GetValue("test"));
-        }
-
-        [TestMethod]
-        public void LoadText_EmptyString()
-        {
-            LoadTextLine("a", "\"\"", false);
-            Assert.AreEqual("", _system.GetValue("a"));
         }
 
         private void LoadTextLine(string variable, string value, bool commented)

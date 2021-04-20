a0cf6cb4aa4120b808a256308ced2bf294a86e91
Robert Kaufmann <robert-kaufmann@live.com>
2021-04-20 17:02:59 +0200

    Add configurable margin between frames

--- MonoKle.Tests/MTextureTests.cs
+++ MonoKle.Tests/MTextureTests.cs
@@ -10,23 +10,46 @@ namespace MonoKle.Tests
         public void Animate_FirstFrame_CorrectAtlasRectangle()
         {
             var sut = new MTexture(null, new MRectangleInt(10, 10, 30, 40), 6, 10);
-            Assert.AreEqual(sut.Animate(0).AtlasRectangle, new MRectangleInt(10, 10, 5, 40));
+            Assert.AreEqual(new MRectangleInt(10, 10, 5, 40), sut.Animate(0).AtlasRectangle);
         }
 
         [TestMethod]
         public void Animate_LastFrame_CorrectAtlasRectangle()
         {
             var sut = new MTexture(null, new MRectangleInt(10, 10, 30, 40), 6, 10);
-            Assert.AreEqual(sut.Animate(5).AtlasRectangle, new MRectangleInt(35, 10, 5, 40));
+            Assert.AreEqual(new MRectangleInt(35, 10, 5, 40), sut.Animate(5).AtlasRectangle);
         }
 
         [TestMethod]
         public void Animate_AfterLastFrame_LoopsAnimation()
         {
             var sut = new MTexture(null, new MRectangleInt(10, 10, 30, 40), 6, 10);
-            Assert.AreEqual(sut.Animate(6).AtlasRectangle, sut.Animate(0).AtlasRectangle);
-            Assert.AreEqual(sut.Animate(11).AtlasRectangle, sut.Animate(5).AtlasRectangle);
-            Assert.AreEqual(sut.Animate(12).AtlasRectangle, sut.Animate(6).AtlasRectangle);
+            Assert.AreEqual(sut.Animate(0).AtlasRectangle, sut.Animate(6).AtlasRectangle);
+            Assert.AreEqual(sut.Animate(5).AtlasRectangle, sut.Animate(11).AtlasRectangle);
+            Assert.AreEqual(sut.Animate(6).AtlasRectangle, sut.Animate(12).AtlasRectangle);
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 12, DisplayName = "First frame")]
+        [DataRow(1, 21, DisplayName = "Second frame")]
+        [DataRow(2, 30, DisplayName = "Last frame")]
+        public void Animate_AtlasMargin_CorrectAtlasRectangle(int frame, int expectedPosX)
+        {
+            const int height = 40;
+            const int posY = 10;
+            var initialAtlas = new MRectangleInt(10, posY, 27, height);
+            var sut = new MTexture(null, initialAtlas, 3, 1, 2);
+            var expectedAtlas = new MRectangleInt(expectedPosX, posY, 5, height);
+            Assert.AreEqual(expectedAtlas, sut.Animate(frame).AtlasRectangle);
+        }
+
+        [TestMethod]
+        public void Animate_AtlasMargin_AfterLastFrame_LoopsAnimation()
+        {
+            var sut = new MTexture(null, new MRectangleInt(10, 10, 30, 40), 6, 10, 2);
+            Assert.AreEqual(sut.Animate(0).AtlasRectangle, sut.Animate(6).AtlasRectangle);
+            Assert.AreEqual(sut.Animate(5).AtlasRectangle, sut.Animate(11).AtlasRectangle);
+            Assert.AreEqual(sut.Animate(6).AtlasRectangle, sut.Animate(12).AtlasRectangle);
         }
 
         [TestMethod]
@@ -64,6 +87,16 @@ namespace MonoKle.Tests
         {
             var sut = new MTexture(null, new MRectangleInt(10, 10, 30, 40), 5, 10);
             Assert.AreEqual(TimeSpan.FromMilliseconds(500), sut.AnimationDuration);
+        }
+
+        [DataTestMethod]
+        [DataRow(14, 2, 2, 3, DisplayName = "2 Margin")] // [][][X][X][X][][][][][Y][Y][Y][][]
+        [DataRow(12, 3, 1, 2, DisplayName = "1 Margin")] // [][X][X][][][Y][Y][][][Z][Z][]
+        [DataRow(6, 3, 0, 2, DisplayName = "0 Margin")]  // [X][X][Y][Y][Z][Z]
+        public void FrameWidth_Correct(int width, int frameCount, int atlasMargin, int expectedResult)
+        {
+            var sut = new MTexture(null, new MRectangleInt(0, 0, width, 5), frameCount, 1, atlasMargin);
+            Assert.AreEqual(expectedResult, sut.FrameWidth);
         }
     }
 }
--- MonoKle/MTexture.cs
+++ MonoKle/MTexture.cs
@@ -28,6 +28,11 @@ namespace MonoKle
         /// </summary>
         public readonly int FrameRate;
 
+        /// <summary>
+        /// The margin between different frames in the atlas. E.g. used for bleed zones in sprite maps or animations.
+        /// </summary>
+        public readonly int AtlasMargin;
+
         public MTexture(Texture2D texture) : this(texture, new MRectangleInt(0, 0, texture.Width, texture.Height)) { }
 
         public MTexture(Texture2D texture, int frameCount, int frameRate)
@@ -36,6 +41,9 @@ namespace MonoKle
         public MTexture(Texture2D texture, MRectangleInt atlasRectangle) : this(texture, atlasRectangle, 1, 1) { }
 
         public MTexture(Texture2D texture, MRectangleInt atlasRectangle, int frameCount, int frameRate)
+            : this(texture, atlasRectangle, frameCount, frameRate, 0) { }
+
+        public MTexture(Texture2D texture, MRectangleInt atlasRectangle, int frameCount, int frameRate, int atlasMargin)
         {
             if (frameCount < 1)
             {
@@ -45,8 +53,14 @@ namespace MonoKle
             {
                 throw new ArgumentException($"Invalid frame rate '{frameRate}'. Must be greater than 0.");
             }
+            if (atlasMargin < 0)
+            {
+                throw new ArgumentException($"Invalid atlas margin '{atlasMargin}'. Must be greater or equal to 0.");
+            }
+
             TextureData = texture;
             AtlasRectangle = atlasRectangle;
+            AtlasMargin = atlasMargin;
             FrameCount = frameCount;
             FrameRate = frameRate;
         }
@@ -57,6 +71,11 @@ namespace MonoKle
         public TimeSpan AnimationDuration => TimeSpan.FromSeconds(FrameCount / (float)FrameRate);
 
         /// <summary>
+        /// Gets the width of a single frame once animated.
+        /// </summary>
+        public int FrameWidth => (AtlasRectangle.Width - AtlasMargin * FrameCount * 2) / FrameCount;
+
+        /// <summary>
         /// Returns new instance of <see cref="MTexture"/> that represents the frame at the provided frame number.
         /// Loops the animation if the provided frame number is greater than <see cref="FrameCount"/> - 1.
         /// </summary>
@@ -64,8 +83,7 @@ namespace MonoKle
         public MTexture Animate(int frameNumber) =>
             new MTexture(TextureData, AtlasRectangle
                 .RedimensionWidth(FrameWidth)
-                .TranslateX(FrameWidth * (frameNumber % FrameCount)));
-        private int FrameWidth => AtlasRectangle.Width / FrameCount;
+                .TranslateX(FrameWidth * (frameNumber % FrameCount) + AtlasMargin * (frameNumber % FrameCount + 1) + AtlasMargin * (frameNumber % FrameCount)));
 
         /// <summary>
         /// Returns new instance of <see cref="MTexture"/> that shows the frame after the provided elapsed time. Loops
@@ -78,6 +96,12 @@ namespace MonoKle
         /// Returns a new instance of <see cref="MTexture"/> with the atlas rectangle set to the provided value.
         /// </summary>
         /// <param name="atlas">The atlas rectangle to use.</param>
-        public MTexture WithAtlas(MRectangleInt atlas) => new MTexture(TextureData, atlas, FrameCount, FrameRate);
+        public MTexture WithAtlas(MRectangleInt atlas) => new MTexture(TextureData, atlas, FrameCount, FrameRate, AtlasMargin);
+
+        /// <summary>
+        /// Returns a new instance of <see cref="MTexture"/> with the atlas margin set to the provided value.
+        /// </summary>
+        /// <param name="margin">The margin to use.</param>
+        public MTexture WithMargin(int margin) => new MTexture(TextureData, AtlasRectangle, FrameCount, FrameRate, margin);
     }
 }

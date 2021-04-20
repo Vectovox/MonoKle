f199991fae4a771bfd673f7c036a8bef24e0bffe
Robert Kaufmann <robert-kaufmann@live.com>
2021-04-20 14:34:41 +0200

    Make set command able to infer default value

--- MonoKle.Engine/Commands/SetCommand.cs
+++ MonoKle.Engine/Commands/SetCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MonoKle.Console;
+using System;
 using System.Collections.Generic;
 
 namespace MonoKle.Engine.Commands
@@ -9,16 +10,40 @@ namespace MonoKle.Engine.Commands
         [ConsolePositional(0, Description = "The variable to set.", IsRequired = true)]
         public string Variable { get; set; }
 
-        [ConsolePositional(1, Description = "The value to assign.", IsRequired = true)]
+        [ConsolePositional(1, Description = "The value to assign.", IsRequired = false)]
         public string Value { get; set; }
 
         public void Call(IGameConsole console)
         {
-            if (MGame.Variables.Variables.Contains(Variable) && MGame.Variables.Variables.CanSet(Variable) == false)
+            // Value is read only
+            if (MGame.Variables.Variables.Contains(Variable) && !MGame.Variables.Variables.CanSet(Variable))
             {
                 console.WriteError("Can not set variable since it is read-only");
+                return;
             }
-            else if (MGame.Variables.VariablePopulator.LoadItem(Variable, Value) == false)
+
+            // Value has not been explicitly provided
+            if (Value == default)
+            {
+                if (MGame.Variables.Variables.TryGetVariable(Variable, out var cvar) && cvar.Type.IsValueType)
+                {
+                    // Automatically set default for existing value types to allow for shorthand:
+                    // set myVariable -> set myVariable true
+                    // set myVariable -> set myVariable 0
+                    var val = cvar.Type == typeof(bool)
+                        ? true
+                        : Activator.CreateInstance(cvar.Type);
+                    MGame.Variables.VariablePopulator.LoadItem(Variable, val.ToString());
+                }
+                else
+                {
+                    console.WriteError("Variable value not provided and type can not be inferred");
+                }
+                return;
+            }
+
+            // Load the provided value
+            if (!MGame.Variables.VariablePopulator.LoadItem(Variable, Value))
             {
                 console.WriteError("Variable assignment failed");
             }

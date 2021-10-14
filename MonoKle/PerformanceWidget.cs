cc700a7155faa3c1994bc22c10f41006be51a701
Robert Kaufmann <robert-kaufmann@live.com>
2021-10-14 19:46:26 +0200

    Upgrade performance widget

--- MonoKle/PerformanceWidget.cs
+++ MonoKle/PerformanceWidget.cs
@@ -8,16 +8,66 @@ using System.Text;
 
 namespace MonoKle
 {
+    public class PerformanceChartData
+    {
+        private readonly float[] _chartData;
+        private int _chartStartIndex;
+        private int _chartEndIndex;
+
+        public int Size => _chartData.Length;
+
+        public PerformanceChartData(int size)
+        {
+            _chartData = new float[size];
+            _chartStartIndex = 0;
+            _chartEndIndex = size - 1;
+        }
+
+        public void FrameComplete(TimeSpan delta)
+        {
+            _chartData[_chartStartIndex] = (float)delta.TotalMilliseconds;
+            IncrementChartPointer(ref _chartStartIndex);
+            IncrementChartPointer(ref _chartEndIndex);
+        }
+
+        public (float, float) GetExtremes()
+        {
+            var maxFrameTime = 0.00001f;
+            var minFrameTime = float.MaxValue;
+
+            for (int i = 0; i < _chartData.Length; i++)
+            {
+                var data = _chartData[i];
+                if (data > maxFrameTime)
+                {
+                    maxFrameTime = data;
+                }
+                if (data < minFrameTime)
+                {
+                    minFrameTime = data;
+                }
+            }
+
+            return (minFrameTime, maxFrameTime);
+        }
+
+        public void IterateData(Action<float, float> callback)
+        {
+            for (int i = _chartStartIndex; i != _chartEndIndex; IncrementChartPointer(ref i))
+            {
+                var firstValue = _chartData[i];
+                var secondValue = _chartData[WrapChartPointer(i + 1)];
+                callback(firstValue, secondValue);
+            }
+        }
+
+        private void IncrementChartPointer(ref int index) => index = WrapChartPointer(index + 1);
+
+        private int WrapChartPointer(int index) => index >= Size ? 0 : index;
+    }
+
     public class PerformanceWidget
     {
-        private const int ChartDataLength = 180;
-        private const int ChartBottom = 185;
-        private const int ChartDelta = 50;
-        private const int ChartTop = ChartBottom - ChartDelta;
-
-        private const float FrameTime30FPS = 33.33f;
-        private const float FrameTime60FPS = 16.66f;
-
         private readonly FrameCounter _drawCounter = new FrameCounter();
         private readonly FrameCounter _updateCounter = new FrameCounter();
         private readonly SpriteBatch _spriteBatch;
@@ -25,10 +75,8 @@ namespace MonoKle
         private readonly FontInstance _font;
         private readonly MTexture _whiteTexture;
         private readonly CachedProcessProvider _processProvider;
-
-        private readonly float[] _chartData = new float[ChartDataLength];
-        private int _chartStartIndex = 0;
-        private int _chartEndIndex = ChartDataLength - 1;
+        private readonly PerformanceChartData _drawChartData = new PerformanceChartData(180);
+        private readonly PerformanceChartData _updateChartData = new PerformanceChartData(180);
 
         // Fields for calculating text with relatively good performance
         private readonly StringBuilder _textBuilder = new StringBuilder();
@@ -65,8 +113,7 @@ namespace MonoKle
         {
             if (Level != MeasurementLevel.Disabled && _drawCounter.IsActive)
             {
-                var time = _drawCounter.End();
-                _chartData[_chartStartIndex] = (float)time.TotalMilliseconds;
+                _drawChartData.FrameComplete(_drawCounter.End());
             }
         }
 
@@ -82,16 +129,9 @@ namespace MonoKle
         {
             if (Level != MeasurementLevel.Disabled && _updateCounter.IsActive)
             {
-                var time = _updateCounter.End();
-                _chartData[_chartStartIndex] += (float)time.TotalMilliseconds;
-                IncrementChartPointer(ref _chartStartIndex);
-                IncrementChartPointer(ref _chartEndIndex);
+                _updateChartData.FrameComplete(_updateCounter.End());
             }
         }
-
-        private void IncrementChartPointer(ref int index) => index = WrapChartPointer(index + 1);
-
-        private int WrapChartPointer(int index) => index >= ChartDataLength ? 0 : index;
 
         public void Draw()
         {
@@ -101,23 +141,12 @@ namespace MonoKle
             }
 
             // Calculate max and min frame times
-            var maxFrameTime = 0.00001f;
-            var minFrameTime = float.MaxValue;
-            foreach (var data in _chartData)
-            {
-                if (data > maxFrameTime)
-                {
-                    maxFrameTime = data;
-                }
-                if (data < minFrameTime)
-                {
-                    minFrameTime = data;
-                }
-            }
+            (float minDrawTime, float maxDrawTime) = _drawChartData.GetExtremes();
+            (float minUpdateTime, float maxUpdateTime) = _updateChartData.GetExtremes();
 
             // Draw the text
             _spriteBatch.Begin();
-            var text = GetText(_updateCounter, _drawCounter, maxFrameTime, minFrameTime, _processProvider);
+            var text = GetText(_updateCounter, _drawCounter, maxDrawTime, minDrawTime, maxUpdateTime, minUpdateTime, _processProvider);
             var textSize = _font.Measure(text);
             _spriteBatch.Draw(_whiteTexture, new MRectangleInt(0, 0, (int)Math.Ceiling(textSize.X), (int)Math.Ceiling(textSize.Y)), new Color(0, 0, 0, 150));
             _font.Draw(_spriteBatch, text, MVector2.Zero, Color.White);
@@ -126,38 +155,47 @@ namespace MonoKle
             // Draw graph
             if (Level >= MeasurementLevel.FrameTime)
             {
-                _primitiveBatch.Begin();
-                var coordinateX = 0;
-
-                float ScaleGraphValue(float value) => (value - minFrameTime) / maxFrameTime * ChartDelta;
-                Color GetGraphColor(float value) => value <= FrameTime60FPS
-                    ? LerpHelper.Lerp(Color.SpringGreen, Color.Yellow, value / FrameTime60FPS)
-                    : LerpHelper.Lerp(Color.Yellow, Color.Red, value / FrameTime30FPS);
-
-                var deltaXPerStep = textSize.X / ChartDataLength;
-
-                // Iterate all points and connect with lines
-                for (int i = _chartStartIndex; i != _chartEndIndex; IncrementChartPointer(ref i))
-                {
-                    var firstValue = _chartData[i];
-                    var secondValue = _chartData[WrapChartPointer(i + 1)];
-                    var firstY = ScaleGraphValue(firstValue);
-                    var secondY = ScaleGraphValue(secondValue);
-                    var firstX = coordinateX * deltaXPerStep;
-                    var secondX = (coordinateX + 1) * deltaXPerStep;
-
-                    _primitiveBatch.DrawLine(new MVector2(firstX, ChartBottom - firstY), new MVector2(secondX, ChartBottom - secondY), GetGraphColor(firstValue), GetGraphColor(secondValue));
-                    coordinateX++;
-                }
-
-                // Draw top and bottom limits
-                _primitiveBatch.DrawLine(new MVector2(0, ChartTop), new MVector2(textSize.X, ChartTop), Color.White);
-                _primitiveBatch.DrawLine(new MVector2(0, ChartBottom), new MVector2(textSize.X, ChartBottom), Color.White);
-                _primitiveBatch.End();
+                DrawGraph(_primitiveBatch, _drawChartData, 167, textSize.X, 50, minDrawTime, maxDrawTime);
+                DrawGraph(_primitiveBatch, _updateChartData, 245, textSize.X, 50, minUpdateTime, maxUpdateTime);
             }
         }
 
-        public string GetText(FrameCounter _updateCounter, FrameCounter _drawCounter, float maxFrameTime, float minFrameTime, CachedProcessProvider processProvider)
+        private static void DrawGraph(PrimitiveBatch2D batch, PerformanceChartData data, float y, float width, float height, float minFrameTime, float maxFrameTime)
+        {
+            const float FrameTime30FPS = 33.33f;
+            const float FrameTime60FPS = 16.66f;
+
+            var bottom = y + height;
+
+            float ScaleGraphValue(float value) => (value - minFrameTime) / maxFrameTime * height;
+            static Color GetGraphColor(float value) => value <= FrameTime60FPS
+                ? LerpHelper.Lerp(Color.SpringGreen, Color.Yellow, value / FrameTime60FPS)
+                : LerpHelper.Lerp(Color.Yellow, Color.Red, value / FrameTime30FPS);
+
+            var deltaXPerStep = width / data.Size;
+            var coordinateX = 0;
+
+            void DrawCallback(float firstValue, float secondValue)
+            {
+                var firstY = ScaleGraphValue(firstValue);
+                var secondY = ScaleGraphValue(secondValue);
+                var firstX = coordinateX * deltaXPerStep;
+                var secondX = (coordinateX + 1) * deltaXPerStep;
+
+                batch.DrawLine(new MVector2(firstX, bottom - firstY), new MVector2(secondX, bottom - secondY), GetGraphColor(firstValue), GetGraphColor(secondValue));
+                coordinateX++;
+            }
+
+            batch.Begin();
+            // Iterate all points and connect with lines
+            data.IterateData(DrawCallback);
+            // Draw top and bottom limits
+            batch.DrawLine(new MVector2(0, y), new MVector2(width, y), Color.White);
+            batch.DrawLine(new MVector2(0, bottom), new MVector2(width, bottom), Color.White);
+            batch.End();
+        }
+
+        public string GetText(FrameCounter _updateCounter, FrameCounter _drawCounter, float maxDrawTime, float minDrawTime, float maxUpdateTime, float minUpdateTime, CachedProcessProvider processProvider)
         {
             var totalFrameTime = _updateCounter.FrameTime + _drawCounter.FrameTime;
 
@@ -168,22 +206,27 @@ namespace MonoKle
                 var theoreticalTotalFPS = (int)(1 / totalFrameTime.TotalSeconds);
 
                 _textBuilder.Clear();
-                _textBuilder.AppendLine($"= FPS: {_drawCounter.FramesPerSecond}");
-                _textBuilder.AppendLine();
+                _textBuilder.AppendLine($"= FRAMES");
+                _textBuilder.AppendLine($"   Draw: {_drawCounter.FramesPerSecond}");
+                _textBuilder.AppendLine($" Update: {_updateCounter.FramesPerSecond}");
 
                 if (Level >= MeasurementLevel.FrameTime)
                 {
+                    _textBuilder.AppendLine();
                     _textBuilder.AppendLine($"= FRAME TIME");
                     _textBuilder.AppendLine($"  Total: {totalFrameTime.TotalMilliseconds:0.00ms} ({theoreticalTotalFPS}/s)");
-                    _textBuilder.AppendLine($" Update: {_updateCounter.FrameTime.TotalMilliseconds:0.00ms} ({_updateCounter.TheoreticalFramesPerSecond}/s)");
                     _textBuilder.AppendLine($"   Draw: {_drawCounter.FrameTime.TotalMilliseconds:0.00ms} ({_drawCounter.TheoreticalFramesPerSecond}/s)");
+                    _textBuilder.AppendLine($" Update: {_updateCounter.FrameTime.TotalMilliseconds:0.00ms} ({_updateCounter.TheoreticalFramesPerSecond}/s)");
                     _textBuilder.AppendLine();
-                    _textBuilder.AppendLine($" MAX: {maxFrameTime:0.00ms}");
+                    _textBuilder.AppendLine($" DRAW {minDrawTime:0.00ms} / {maxDrawTime:0.00ms}");
                     _textBuilder.AppendLine();
                     _textBuilder.AppendLine();
                     _textBuilder.AppendLine();
                     _textBuilder.AppendLine();
-                    _textBuilder.AppendLine($" MIN: {minFrameTime:0.00ms}");
+                    _textBuilder.AppendLine($" UPDATE: {minUpdateTime:0.00ms} / {maxUpdateTime:0.00ms}");
+                    _textBuilder.AppendLine();
+                    _textBuilder.AppendLine();
+                    _textBuilder.AppendLine();
                 }
 
                 if (Level >= MeasurementLevel.Process)

338294d35464e52a695ebd00f06b79d0342a36e8
Robert Kaufmann <robert-kaufmann@live.com>
2021-12-08 19:24:39 +0100

    Add cyclic timer type

---
+++ MonoKle.Tests/CyclicTimerTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MonoKle.Tests
+{
+    [TestClass]
+    public class CyclicTimerTests
+    {
+        [TestMethod]
+        public void Constructor_DurationSet()
+        {
+            var sut = new CyclicTimer(TimeSpan.FromSeconds(63));
+            Assert.AreEqual(63, sut.Duration.TotalSeconds);
+        }
+
+        [TestMethod]
+        public void Constructor_ZeroDuration_Exception()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new CyclicTimer(TimeSpan.Zero));
+        }
+
+        [TestMethod]
+        public void SetDuration_ZeroDuration_Exception()
+        {
+            var sut = new CyclicTimer(TimeSpan.FromSeconds(1));
+            Assert.ThrowsException<ArgumentException>(() => sut.Duration = TimeSpan.Zero);
+        }
+
+        [DataTestMethod]
+        [DataRow(0, 0, DisplayName = "No time passed")]
+        [DataRow(1999, 0, DisplayName = "Not enough time passed")]
+        [DataRow(2000, 1, DisplayName = "Exactly one cycle passed")]
+        [DataRow(3999, 1, DisplayName = "Almost two cycles passed")]
+        [DataRow(4000, 2, DisplayName = "Two cycles passed")]
+        [DataRow(6000, 3, DisplayName = "Three cycles passed")]
+        public void Update_CorrectCyclesReturned(int millisecondsUpdated, int expectedCycles)
+        {
+            var sut = new CyclicTimer(TimeSpan.FromSeconds(2));
+            Assert.AreEqual(expectedCycles, sut.Update(TimeSpan.FromMilliseconds(millisecondsUpdated)));
+        }
+
+        [TestMethod]
+        public void Reset_DoesNotTrigger()
+        {
+            var sut = new CyclicTimer(TimeSpan.FromSeconds(1));
+            sut.Update(TimeSpan.FromMilliseconds(999));
+            sut.Reset();
+            Assert.AreEqual(0, sut.Update(TimeSpan.FromMilliseconds(999)));
+        }
+    }
+}
---
+++ MonoKle/CyclicTimer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MonoKle
+{
+    /// <summary>
+    /// Timer that allows multiple cycles (triggers) per update. For instance useful
+    /// when creating frame rate independent systems.
+    /// </summary>
+    public class CyclicTimer
+    {
+        private TimeSpan _elapsed = TimeSpan.Zero;
+        private TimeSpan _duration;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="CyclicTimer"/> with the given duration.
+        /// </summary>
+        /// <param name="duration">The duration to use. Must not be zero.</param>
+        /// <exception cref="ArgumentException">Thrown if duration is zero.</exception>
+        public CyclicTimer(TimeSpan duration) => Duration = duration;
+
+        /// <summary>
+        /// Gets or sets the duration of the timer. Must not be zero.
+        /// </summary>
+        public TimeSpan Duration
+        {
+            get => _duration;
+            set
+            {
+                if (value == TimeSpan.Zero)
+                {
+                    throw new ArgumentException("Duration must be greated than zero.");
+                }
+                _duration = value;
+            }
+        }
+
+        /// <summary>
+        /// Updates the <see cref="CyclicTimer"/>, returning the amount of cycles
+        /// that are triggered.
+        /// </summary>
+        /// <param name="timeDelta">The amount of time passed.</param>
+        /// <returns>The amount of cycles.</returns>
+        public int Update(TimeSpan timeDelta)
+        {
+            _elapsed += timeDelta;
+
+            var triggers = 0;
+            while (_elapsed >= _duration)
+            {
+                _elapsed -= _duration;
+                triggers++;
+            }
+
+            return triggers;
+        }
+
+        /// <summary>
+        /// Resets the timer to its intial state of zero elapsed time.
+        /// </summary>
+        public void Reset() => _elapsed = TimeSpan.Zero;
+    }
+}

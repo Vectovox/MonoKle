2fe6cc26c59118032c5b07c4948a294c9235e5fd
Robert Kaufmann <robert-kaufmann@live.com>
2023-01-28 15:27:24 +0100

    Add app settings for company and product

--- MonoKle.Engine/GameDataStorage.cs
+++ MonoKle.Engine/GameDataStorage.cs
@@ -1,5 +1,6 @@
 ﻿using MonoKle.Configuration;
 using System;
+using System.Configuration;
 using System.IO;
 
 namespace MonoKle.Engine
@@ -7,17 +8,12 @@ namespace MonoKle.Engine
     public static class GameDataStorage
     {
         private static Environment.SpecialFolder _root = Environment.SpecialFolder.LocalApplicationData;
-        private static string _companyName = "MonoKle";
-        private static string _projectName = "Demo";
-        private static readonly string _logFolderName = "Logs";
         private static bool _dirty = true;
 
         private static string _cachedProjectPath;
         private static string _cachedLogsPath;
 
         public static Environment.SpecialFolder Root { get => _root; set { _root = value; _dirty = true; } }
-        public static string Company { get => _companyName; set { _companyName = value; _dirty = true; } }
-        public static string Project { get => _projectName; set { _projectName = value; _dirty = true; } }
 
         [CVar("projectDataPath")]
         public static string ProjectPath { get => GetProjectPath(); }
@@ -45,14 +41,17 @@ namespace MonoKle.Engine
             {
                 _dirty = false;
 
+                var company = ConfigurationManager.AppSettings["company"] ?? "MonoKle";
+                var project = ConfigurationManager.AppSettings["product"] ?? "Demo";
+
                 var root = Environment.GetFolderPath(Root);
                 if (string.IsNullOrWhiteSpace(root))
                 {
                     throw new InvalidOperationException($"Root path not accessible ({Root})!");
                 }
 
-                _cachedProjectPath = Path.Combine(Path.Combine(root, Company), Project);
-                _cachedLogsPath = Path.Combine(_cachedProjectPath, _logFolderName);
+                _cachedProjectPath = Path.Combine(Path.Combine(root, company), project);
+                _cachedLogsPath = Path.Combine(_cachedProjectPath, "Logs");
             }
         }
     }

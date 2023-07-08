5a76f557cef85e1a7819b3e2e45944382da269c6
Robert Kaufmann <robert-kaufmann@live.com>
2023-07-08 16:02:39 +0200

    Add display mode dimensions extension

---
+++ MonoKle/Graphics/DisplayModeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoKle.Graphics
+{
+    public static class DisplayModeExtensions
+    {
+        public static MPoint2 Dimensions(this DisplayMode mode) => new(mode.Width, mode.Height);
+    }
+}
